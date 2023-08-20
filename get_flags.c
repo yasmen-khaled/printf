fd250c3f191ccee1218cb2e69159c7b1c3700ec1
yasmen <yasmen.khaled2000@gmail.com>
2023-08-20 03:38:54 +0200

    test

---
+++ get_flags.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * get_flags - Calculates
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ */
+
+
+int get_flags(const char *format, int *i)
+{
+	int x, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (x = 0; FLAGS_CH[x] != '\0'; x++)
+			if (format[curr_i] == FLAGS_CH[x])
+			{
+				flags |= FLAGS_ARR[x];
+				break;
+			}
+
+		if (FLAGS_CH[x] == 0)
+			break;
+	}
+
+	*i = curr_i - 1;
+
+	return (flags);
+}
